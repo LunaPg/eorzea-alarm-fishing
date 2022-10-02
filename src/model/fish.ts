dc5e877087175fd16d0c242df041952ca61123bc
luna <malalunapg@gmail.com>
2022-10-01 21:32:02 -0400

    feat(fish): add model

---
+++ src/model/fish.ts
@@ -0,0 +1,23 @@
+export class Fish {
+  private name: string;
+  private EZTime: number;
+  private IRLTime: number;
+
+  constructor(name: string, EZTime: number, IRLTime: number) {
+    this.name = name;
+    this.EZTime = EZTime;
+    this.IRLTime = IRLTime;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getEZTime(): number {
+    return this.EZTime;
+  }
+
+  public getIRLTime(): number {
+    return this.IRLTime;
+  }
+}
